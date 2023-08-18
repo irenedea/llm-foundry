c98d0810c174ee0c87d2263e087b73cae4f33128
Irene Dea <irene.dea@databricks.com>
2023-08-18 18:38:14 +0000

    task items are addressed

--- llmfoundry/callbacks/generate_callback.py
+++ llmfoundry/callbacks/generate_callback.py
@@ -13,12 +13,13 @@ from composer.utils import dist, ensure_tuple
 from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 from composer.callbacks import checkpoint_periodically
 
-Tokenizer = Union[PreTrainedTokenizer, PreTrainedTokenizerFast]
+from torch.utils.data import TensorDataset, DataLoader
 
+Tokenizer = Union[PreTrainedTokenizer, PreTrainedTokenizerFast]
 
 class Generate(Callback):
 
-    def __init__(self, prompts: List[str], interval: Union[str, int, Time],
+    def __init__(self, prompts: List[str], interval: Union[str, int, Time], batch_size: int,
                  **kwargs: Any):
         """Periodically log generations to wandb from a set of prompts.
 
@@ -43,36 +44,30 @@ class Generate(Callback):
         """
         self.prompts = prompts
         self.generate_kwargs = kwargs
-        self.wandb_logger = None
+        self.batch_size = batch_size
         self.save_interval = checkpoint_periodically(interval)
 
     def init(self, state: State, logger: Logger):
         assert isinstance(state.model, HuggingFaceModel)
-        if dist.get_global_rank() == 0:
-            # raise Exception()
-            for destination in ensure_tuple(logger.destinations):
-                if isinstance(destination, WandBLogger):
-                    self.wandb_logger = destination
 
-    def batch_checkpoint(self, state: State, logger: Logger):
-        print('HEY', checkpoint_periodically(state, Event.BATCH_CHECKPOINT))
-        if (state.timestamp.batch.value % self.batch_log_interval) == 0:
-            raise Exception()
+    def run_event(self, event: Event, state: State, logger: Logger) -> None:
+        if self.save_interval(state, event): 
             self.generate(state, logger)
+        else:
+            super().run_event(event, state, logger)
 
     def generate(self, state: State, logger: Logger):
         model = state.model
+        assert isinstance(model, HuggingFaceModel) # TODO: Extend to support any models that have a generate method.
+
+        tokenizer = state.model.tokenizer
+        assert isinstance(tokenizer, Tokenizer)
+
+        # Set to evaluation mode and stash the original mode.
         original_mode = model.training
         model.eval()
-        tokenizer = cast(Tokenizer, state.model.tokenizer)
-        device = state.device
 
-        if not hasattr(model.model, 'generate'):
-            raise ValueError(
-                f'Cannot generate from model {model.model.__class__.__name__} because it does not have a `generate` method'
-            )
-
-        # stash the original original value of padding_side because generation requires left padding
+        # Stash the original value of padding_side because generation requires left padding
         original_padding_side = tokenizer.padding_side
         tokenizer.padding_side = 'left'
         if tokenizer.pad_token_id is None:
@@ -81,48 +76,36 @@ class Generate(Callback):
                                     return_tensors='pt',
                                     padding=True)
 
-        for k, v in tokenized_input.items():
-            tokenized_input[k] = device.tensor_to_device(v)
 
-        # dummy forward call needed for FSDP to work consistently
-        dummy_input = torch.tensor([[0]], dtype=torch.long)
-        dummy_input = device.tensor_to_device(dummy_input)
-        with get_precision_context(state.precision):
-            with torch.no_grad():
-                assert isinstance(model.model, torch.nn.Module)
-                _ = model.model(input_ids=dummy_input)
+        all_input_ids = tokenized_input['input_ids']
+        all_attn_masks = tokenized_input['attention_mask']
+        batches = DataLoader(TensorDataset(all_input_ids, all_attn_masks), self.batch_size)
 
-            output_token_ids = model.model.generate(  # type: ignore
-                input_ids=tokenized_input['input_ids'],
-                attention_mask=tokenized_input['attention_mask'],
+        device = state.device
+        output_token_ids = []
+        for input_ids, attention_mask in batches:
+            # Move batch to device.
+            input_ids = device.tensor_to_device(input_ids)
+            attention_mask = device.tensor_to_device(attention_mask)
+
+            # Generate outputs and add to list.
+            output_token_ids.extend(model.generate(  # type: ignore
+                input_ids=input_ids,
+                attention_mask=attention_mask,
                 synced_gpus=True,
                 **self.generate_kwargs,
-            )
+            ))
 
         if dist.get_global_rank() == 0:
-            if self.wandb_logger is not None:
-                assert wandb.run is not None, 'wandb should have started run'
+            # Process prompts and outputs into a table.
+            rows = []
+            input_tokens_len = all_input_ids.shape[1]
+            for i, prompt in enumerate(self.prompts):
+                output_tokens = output_token_ids[i][input_tokens_len:]
+                output_text = tokenizer.decode(output_tokens, skip_special_tokens=True)
+                rows.append([prompt, output_text])
 
-                artifact = wandb.Artifact('generate_samples_' +
-                                          str(wandb.run.id),
-                                          type='predictions')
-
-                rows = []
-                for i in range(len(self.prompts)):
-                    prompt = self.prompts[i]
-                    output_tokens = output_token_ids[i][
-                        tokenized_input['input_ids'].shape[1]:]
-                    output_text = tokenizer.decode(output_tokens,
-                                                   skip_special_tokens=True)
-
-                    rows.append([prompt, output_text])
-
-                text_table = wandb.Table(data=rows,
-                                         columns=['prompt', 'generation'])
-                artifact.add(text_table, 'predictions')
-                wandb.log_artifact(artifact)
-                wandb.log({'generations': text_table},
-                          step=state.timestamp.batch.value)
+            # TODO: LOG TABLE
 
         tokenizer.padding_side = original_padding_side
         model.train(mode=original_mode)
