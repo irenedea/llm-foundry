61902b545bf2dba3e887897f292e3029004a0fa7
Irene Dea <deaairene@gmail.com>
2024-07-17 22:58:31 +0000

    Add transform model for hf_causal_lm

--- llmfoundry/models/hf/hf_causal_lm.py
+++ llmfoundry/models/hf/hf_causal_lm.py
@@ -108,6 +108,8 @@ class ComposerHFCausalLM(HuggingFaceModelWithFSDP):
             prepare_for_fsdp=True,
         )
 
+        model = self.transform_model(model)
+
         train_metrics, eval_metrics = ComposerHFCausalLM.build_metrics(
             use_train_metrics=use_train_metrics,
             additional_train_metrics=additional_train_metrics,
@@ -134,6 +136,17 @@ class ComposerHFCausalLM(HuggingFaceModelWithFSDP):
             peft_config=peft_config_object,
             should_save_peft_only=should_save_peft_only,
         )
+
+    def transform_model(self, model: PreTrainedModel) -> PreTrainedModel:
+        """Transforms the HF model.
+
+        Args:
+            model (PreTrainedModel): The model to transform.
+
+        Returns:
+            PreTrainedModel: The transformed model.
+        """
+        return model
 
     @staticmethod
     def build_metrics(
